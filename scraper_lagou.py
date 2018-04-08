7996a8d14f083fc73306f4a1082ac945b694e807
AUTHENTIC <leogwork@gmail.com>
2018-04-08 10:42:06 +0800

    LaGou Web Scraper

--- scraper_lagou.py
+++ scraper_lagou.py
@@ -1,8 +1,15 @@
 import urllib.request
+from bs4 import BeautifulSoup
 import re
 
-def main():
-    urllib.request('http://www.lagou.com')
+def getPage(url):
+    response = urllib.request.urlopen(url)
+    html = response.read().decode('utf-8')
+    soup = BeautifulSoup(html, 'html.parser')
 
-if "__init__" == "__main__":
+def main():
+    web_address = 'http://www.lagou.com'
+    getPage(web_address)
+
+if __name__ == "__main__":
     main()
