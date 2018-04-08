62bfad37908ad91284b17f390f9eb8f40902a434
AUTHENTIC <leogwork@gmail.com>
2018-04-08 15:32:01 +0800

    LaGou Web Scraper

--- scraper_lagou.py
+++ scraper_lagou.py
@@ -6,9 +6,14 @@ def getPage(city):
         'pn': 1,
         'kd': '测试'
     }
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+        'Referer': 'https://www.lagou.com/jobs/list_%E6%B5%8B%E8%AF%95?city=%E6%88%90%E9%83%BD&cl=false&fromSearch=true&labelWords=&suginput='
+    }
     url_position = 'https://www.lagou.com/jobs/positionAjax.json?city={0}needAddtionalResult=false'.format(city)
-    r = requests.post(url_position, data=fromdata)
-    print(r.text)
+    r = requests.post(url_position, data=fromdata, headers=headers)
+    result = r.json()['content']['positionResult']['result']
+    print(result)
 
 def main():
     city = input('选择城市：')
